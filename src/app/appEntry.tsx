3cfc4fefb73d600cc84bae09c6524e8c6c098d44
Sergey Peshkov <zloi.g1rz@gmail.com>
2024-09-13 00:03:32 +0800

    add base styles

--- src/app/appEntry.tsx
+++ src/app/appEntry.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
+import '~/shared/styles/index.scss';
+
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <p>test</p>
