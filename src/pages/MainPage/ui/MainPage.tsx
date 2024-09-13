f66ff257529e490b4cd6c40ccb45556264a6113d
Sergey Peshkov <zloi.g1rz@gmail.com>
2024-09-14 00:00:16 +0800

    add react-helmet

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,8 +1,13 @@
 // import styles from './MainPage.module.scss';
 
+import { Helmet } from 'react-helmet';
+
 export function MainPage() {
     return (
         <>
+            <Helmet>
+                <title>Catalog | Goods4you</title>
+            </Helmet>
             <p>
                 Lorem, ipsum dolor sit amet consectetur adipisicing elit.
                 Molestiae deleniti dolores voluptas dolorum, sit, quae at
