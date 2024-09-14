17fc17f0fb7f32a454dd793392bc426c33e83838
Sergey Peshkov <zloi.g1rz@gmail.com>
2024-09-14 16:09:11 +0800

    add footer content

--- src/widgets/Footer/Footer.tsx
+++ src/widgets/Footer/Footer.tsx
@@ -1,9 +1,22 @@
+import { AppLink, AppLogo, Container } from '~/shared/ui';
 import styles from './Footer.module.scss';
 
 export function Footer() {
     return (
         <footer className={styles.footer}>
-            <p>test</p>
+            <Container>
+                <div className={styles.row}>
+                    <AppLogo />
+                    <div className={styles.menu}>
+                        <AppLink to="/#catalog" type={'white'}>
+                            Catalog
+                        </AppLink>
+                        <AppLink to="/#faq" type={'white'}>
+                            FAQ
+                        </AppLink>
+                    </div>
+                </div>
+            </Container>
         </footer>
     );
 }
