3f547c473e9b0be2c3097a9b58d70fab7105891c
Sergey Peshkov <zloi.g1rz@gmail.com>
2024-09-15 01:49:56 +0800

    fix types

--- types/app.d.ts
+++ types/app.d.ts
@@ -1,6 +1,6 @@
 declare global {
     type Size = 's' | 'm' | 'l' | 'xl' | '2xl' | '3xl';
-    type FontWeight = 'bold' | 'semibild' | 'regular';
+    type FontWeight = 'bold' | 'semibold' | 'regular';
 }
 
 export {};
