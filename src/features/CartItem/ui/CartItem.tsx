0e6d135c6ecbf32c1e68207cd2108575132ffce4
Sergey Peshkov <zloi.g1rz@gmail.com>
2024-09-15 20:07:36 +0800

    add CartItem feature

---
+++ src/features/CartItem/ui/CartItem.tsx
@@ -0,0 +1,59 @@
+import { AddedControl } from '~/features/AddedControl';
+import styles from './CartItem.module.scss';
+import { AppLink, Button, Text } from '~/shared/ui';
+import { IconCart } from '~/shared/ui/Icons';
+import clsx from 'clsx';
+
+type Props = {
+    id: number;
+    title: string;
+    preview: string;
+    price: string;
+    count: number;
+    isDeleted: boolean;
+};
+
+export function CartItem({
+    id,
+    title,
+    preview,
+    price,
+    count,
+    isDeleted,
+}: Props) {
+    return (
+        <div className={clsx([styles.card, isDeleted && styles.deleted])}>
+            <div className={styles.left}>
+                <div className={styles.preview}>
+                    <img src={preview} alt={title} />
+                </div>
+                <div className={styles.content}>
+                    <Text
+                        as={AppLink}
+                        to={`/product/${id}`}
+                        size="m"
+                        weight="semibold"
+                        type="currentColor"
+                    >
+                        {title}
+                    </Text>
+                    <Text as="span" size="l">
+                        {price}
+                    </Text>
+                </div>
+            </div>
+            <div className={styles.right}>
+                {isDeleted ? (
+                    <Button onlyIcon>
+                        <IconCart />
+                    </Button>
+                ) : (
+                    <>
+                        <AddedControl initialCount={count} />
+                        <AppLink className={styles.deleteLink}>Delete</AppLink>
+                    </>
+                )}
+            </div>
+        </div>
+    );
+}
