57ee8f9dfef01eb69231fabf42c483f4dd0ae9ec
Sergey Peshkov <zloi.g1rz@gmail.com>
2024-09-15 14:16:31 +0800

    add width prop in Container

--- src/shared/ui/Container/Container.tsx
+++ src/shared/ui/Container/Container.tsx
@@ -1,10 +1,16 @@
 import { ReactNode } from 'react';
 import styles from './Container.module.scss';
+import { clsx } from 'clsx';
 
 type ContainerProps = {
     children?: ReactNode;
+    width?: 'min' | 'regular';
 };
 
-export function Container(props: ContainerProps) {
-    return <div className={styles.container}>{props.children}</div>;
+export function Container({ children, width = 'regular' }: ContainerProps) {
+    return (
+        <div className={clsx([styles.container, styles[width]])}>
+            {children}
+        </div>
+    );
 }
