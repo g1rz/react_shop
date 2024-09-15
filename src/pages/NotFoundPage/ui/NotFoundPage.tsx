ff0b44db3ef678659520883dbba8957eb1315583
Sergey Peshkov <zloi.g1rz@gmail.com>
2024-09-15 22:15:56 +0800

    add content to 404 page

--- src/pages/NotFoundPage/ui/NotFoundPage.tsx
+++ src/pages/NotFoundPage/ui/NotFoundPage.tsx
@@ -1,5 +1,26 @@
-// import styles from './NotFoundPage.module.scss';
+import { Button, Container, Text } from '~/shared/ui';
+import styles from './NotFoundPage.module.scss';
 
 export function NotFoundPage() {
-    return <div>404Page</div>;
+    return (
+        <div className={styles.page}>
+            <Container>
+                <div className={styles.inner}>
+                    <Text
+                        className={styles.title}
+                        as="h1"
+                        weight="bold"
+                        size="3xl"
+                    >
+                        404 Page not found
+                    </Text>
+                    <img
+                        src="/src/pages/NotFoundPage/assets/travolta.gif"
+                        alt="page 404"
+                    />
+                    <Button to="/">Back to main</Button>
+                </div>
+            </Container>
+        </div>
+    );
 }
