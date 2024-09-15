a1455e84f45127fff5d2599c6134dfbbbe3ae1f7
Sergey Peshkov <zloi.g1rz@gmail.com>
2024-09-15 19:48:42 +0800

    add button to AppLink

--- src/shared/ui/AppLink/AppLink.tsx
+++ src/shared/ui/AppLink/AppLink.tsx
@@ -4,20 +4,36 @@ import styles from './AppLink.module.scss';
 import { ReactNode } from 'react';
 import clsx from 'clsx';
 
-type LinkTypes = 'white' | 'gray';
+type LinkTypes = 'white' | 'gray' | 'currentColor';
 
 type AppLinkProps = {
-    to: string;
+    to?: string;
     children: ReactNode;
     className?: string;
     type?: LinkTypes;
+    onClick?: () => void;
 };
 
 export function AppLink(props: AppLinkProps) {
-    const { to, children, className, type = 'gray' } = props;
+    const { to, children, className, type = 'gray', onClick } = props;
+
+    if (to) {
+        return (
+            <Link
+                to={to}
+                className={clsx([styles.link, className, styles[type]])}
+            >
+                {children}
+            </Link>
+        );
+    }
+
     return (
-        <Link to={to} className={clsx([styles.link, className, styles[type]])}>
+        <button
+            className={clsx([styles.link, styles.btn, className, styles[type]])}
+            onClick={onClick}
+        >
             {children}
-        </Link>
+        </button>
     );
 }
