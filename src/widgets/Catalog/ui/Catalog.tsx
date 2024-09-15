26e0925b5ca2a3e1076f322e44071158043ebed8
Sergey Peshkov <zloi.g1rz@gmail.com>
2024-09-15 14:04:40 +0800

    add content to widget Catalog

--- src/widgets/Catalog/ui/Catalog.tsx
+++ src/widgets/Catalog/ui/Catalog.tsx
@@ -1,4 +1,4 @@
-import { Container, TextField } from '~/shared/ui';
+import { Button, Container, TextField } from '~/shared/ui';
 import { Text } from '~/shared/ui';
 import { ProductCard } from '~/entities/Product';
 
@@ -16,50 +16,132 @@ const mockProducts = [
         price: '$110',
         isAdded: true,
     },
+    {
+        id: 2,
+        preview: '/src/entities/product/assets/product-1.png',
+        title: 'Essence Mascara Lash Princess',
+        price: '$110',
+        isAdded: false,
+    },
+    {
+        id: 3,
+        preview: '/src/entities/product/assets/product-1.png',
+        title: 'Essence Mascara Lash Princess',
+        price: '$110',
+        isAdded: false,
+    },
+    {
+        id: 4,
+        preview: '/src/entities/product/assets/product-1.png',
+        title: 'Essence Mascara Lash Princess',
+        price: '$110',
+        isAdded: false,
+    },
+    {
+        id: 5,
+        preview: '/src/entities/product/assets/product-1.png',
+        title: 'Essence Mascara Lash Princess',
+        price: '$110',
+        isAdded: false,
+    },
+    {
+        id: 6,
+        preview: '/src/entities/product/assets/product-1.png',
+        title: 'Essence Mascara Lash Princess',
+        price: '$110',
+        isAdded: false,
+    },
+    {
+        id: 7,
+        preview: '/src/entities/product/assets/product-1.png',
+        title: 'Essence Mascara Lash Princess',
+        price: '$110',
+        isAdded: false,
+    },
+    {
+        id: 8,
+        preview: '/src/entities/product/assets/product-1.png',
+        title: 'Essence Mascara Lash Princess',
+        price: '$110',
+        isAdded: false,
+    },
+    {
+        id: 9,
+        preview: '/src/entities/product/assets/product-1.png',
+        title: 'Essence Mascara Lash Princess',
+        price: '$110',
+        isAdded: false,
+    },
+    {
+        id: 10,
+        preview: '/src/entities/product/assets/product-1.png',
+        title: 'Essence Mascara Lash Princess',
+        price: '$110',
+        isAdded: false,
+    },
+    {
+        id: 11,
+        preview: '/src/entities/product/assets/product-1.png',
+        title: 'Essence Mascara Lash Princess',
+        price: '$110',
+        isAdded: false,
+    },
+    {
+        id: 12,
+        preview: '/src/entities/product/assets/product-1.png',
+        title: 'Essence Mascara Lash Princess',
+        price: '$110',
+        isAdded: false,
+    },
 ];
 
 export function Catalog({}: Props) {
     const [search, setSearch] = useState('');
 
     return (
-        <div className={styles.catalog}>
+        <div className={styles.catalog} id="catalog">
             <Container>
-                <Text
-                    as={'h2'}
-                    size={'3xl'}
-                    weight={'bold'}
-                    className={styles.title}
-                >
-                    Catalog
-                </Text>
-                <TextField
-                    value={search}
-                    onChange={(e) => setSearch(e.target.value)}
-                    placeholder="Search by title"
-                    className={styles.search}
-                />
-                <div className={styles.grid}>
-                    {mockProducts.map(
-                        ({ id, preview, title, price, isAdded }) => (
-                            <ProductCard
-                                key={id}
-                                id={id}
-                                preview={preview}
-                                title={title}
-                                price={price}
-                                isAdded={isAdded}
-                                renderControl={({
-                                    initialCount,
-                                    onCountChange,
-                                }) => (
-                                    <AddedControl
-                                        initialCount={initialCount}
-                                        onCountChange={onCountChange}
-                                    />
-                                )}
-                            />
-                        ),
-                    )}
+                <div className={styles.catalogInner}>
+                    <Text
+                        as={'h2'}
+                        size={'3xl'}
+                        weight={'bold'}
+                        className={styles.title}
+                    >
+                        Catalog
+                    </Text>
+                    <TextField
+                        value={search}
+                        onChange={(e) => setSearch(e.target.value)}
+                        placeholder="Search by title"
+                        className={styles.search}
+                    />
+                    <div className={styles.grid}>
+                        {mockProducts.map(
+                            ({ id, preview, title, price, isAdded }) => (
+                                <ProductCard
+                                    key={id}
+                                    id={id}
+                                    preview={preview}
+                                    title={title}
+                                    price={price}
+                                    isAdded={isAdded}
+                                    renderControl={({
+                                        initialCount,
+                                        onCountChange,
+                                    }) => (
+                                        <AddedControl
+                                            initialCount={initialCount}
+                                            onCountChange={onCountChange}
+                                        />
+                                    )}
+                                />
+                            ),
+                        )}
+                    </div>
+                    <div className={styles.showMore}>
+                        <Button>Show more</Button>
+                    </div>
                 </div>
             </Container>
         </div>
