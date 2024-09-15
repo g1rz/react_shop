83cea3fec7dc723461294c98e7c73993c60d0daf
Sergey Peshkov <zloi.g1rz@gmail.com>
2024-09-15 22:45:15 +0800

    add adaptive for header

--- src/shared/ui/Icons/index.ts
+++ src/shared/ui/Icons/index.ts
@@ -3,3 +3,5 @@ export { IconMinus } from './IconMinus/IconMinus';
 export { IconCart } from './IconCart/IconCart';
 export { IconCross } from './IconCross/IconCross';
 export { IconStar } from './IconStar/IconStar';
+export { IconBurger } from './IconBurger/IconBurger';
+export { IconClose } from './IconClose/IconClose';
--- src/widgets/Header/ui/Header/Header.tsx
+++ src/widgets/Header/ui/Header/Header.tsx
@@ -1,15 +1,33 @@
-import { AppLink, AppLogo, Container } from '~/shared/ui/';
+import { AppLink, AppLogo, Button, Container } from '~/shared/ui/';
 
 import styles from './Header.module.scss';
 import { CartButton } from '../CartButton/CartButton';
+import { IconBurger, IconClose } from '~/shared/ui/Icons';
+import { useState } from 'react';
+import clsx from 'clsx';
 
 export function Header() {
+    const [isOpenMenu, setIsOpenMenu] = useState(false);
     return (
         <header className={styles.header}>
             <Container>
                 <div className={styles.row}>
                     <AppLogo />
-                    <nav className={styles.menu}>
+                    <Button
+                        className={styles.burger}
+                        onlyIcon
+                        onClick={() =>
+                            setIsOpenMenu((isOpenMenu) => !isOpenMenu)
+                        }
+                    >
+                        {isOpenMenu ? <IconClose /> : <IconBurger />}
+                    </Button>
+                    <nav
+                        className={clsx([
+                            styles.menu,
+                            isOpenMenu && styles.isOpen,
+                        ])}
+                    >
                         <AppLink to="/#catalog" type={'white'}>
                             Catalog
                         </AppLink>
