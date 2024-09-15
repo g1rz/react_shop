1bcd5a7ed85482d43c25dca69202a416f5bf44bc
Sergey Peshkov <zloi.g1rz@gmail.com>
2024-09-15 21:26:15 +0800

    add favicon

--- src/app/layout/baselayout.tsx
+++ src/app/layout/baselayout.tsx
@@ -2,9 +2,13 @@ import { Layout } from '~/shared/ui';
 import { Footer } from '~/widgets/Footer';
 import { Header } from '~/widgets/Header';
 import ScrollToAnchor from './ScrollToAnchor';
+import { Helmet } from 'react-helmet-async';
 
 export const baselayout = (
     <>
+        <Helmet>
+            <link rel="icon" href="/public/favicon.svg" />
+        </Helmet>
         <ScrollToAnchor />
         <Layout headerSlot={<Header />} footerSlot={<Footer />} />
     </>
