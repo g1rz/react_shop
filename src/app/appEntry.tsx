a3512bcfe2a8809dd37938d9e078e23ad8c146d1
Sergey Peshkov <zloi.g1rz@gmail.com>
2024-09-22 00:52:24 +0800

    add redux provider

--- src/app/appEntry.tsx
+++ src/app/appEntry.tsx
@@ -1,15 +1,19 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
+import { Provider as ReduxProvider } from 'react-redux';
+import { HelmetProvider } from 'react-helmet-async';
 
 import '~/shared/styles/index.scss';
 import { appRouter } from './appRouter';
-import { HelmetProvider } from 'react-helmet-async';
+import { store } from './appStore';
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <HelmetProvider>
-            <RouterProvider router={appRouter()} />
-        </HelmetProvider>
+        <ReduxProvider store={store}>
+            <HelmetProvider>
+                <RouterProvider router={appRouter()} />
+            </HelmetProvider>
+        </ReduxProvider>
     </StrictMode>,
 );
