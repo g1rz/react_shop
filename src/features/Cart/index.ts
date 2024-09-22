c4b8666c3c80861d08f5d4edc5f1988868534d40
Sergey Peshkov <zloi.g1rz@gmail.com>
2024-09-22 18:46:30 +0800

    add cartSlice

---
+++ src/features/Cart/index.ts
@@ -0,0 +1,8 @@
+export { CartItem } from './ui/CartItem/CartItem';
+export { type CartItemProps, type CartResponse } from './model/types';
+export { default as cartReducer } from './model/cartSlice';
+export { selectCartItems } from './model/cartSlice';
+export { selectCartTotalQuantity } from './model/cartSlice';
+export { selectIsProductInCart } from './model/cartSlice';
+export { fetchCart } from './model/cartSlice';
+export { selectProductCountInCart } from './model/cartSlice';
